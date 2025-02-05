3c51a73b0ea7d4c10b635fb282a6d1a338981937
Kyle553 <elkorda204@gmail.com>
2025-02-06 01:55:07 +0200

    Урок 4 - 5

--- Модуль 1 Основи JavaScript/Урок 4/script.js
+++ Модуль 1 Основи JavaScript/Урок 4/script.js
@@ -8,7 +8,15 @@ arr.push(Math.floor(Math.random() * 101));
 console.log(("Розмір Контейнера:"), size);
 
 function sort() {
-  arr.sort((a, b) => a - b);
+  for (let i = 0; i <= arr.length; i++) {
+    for (let j = 0; j <= arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
   console.log(arr);
   console.log(("Мінімальне число:"), arr[0]);
 }
@@ -16,15 +24,7 @@ function sort() {
 sort();
 
 // function sort() {
-//   for (let i = 0; i <= arr.length; i++) {
-//     for (let j = 0; j <= arr.length; j++) {
-//       if (arr[j] > arr[j + 1]) {
-//         let temp = arr[j + 1];
-//         arr[j + 1] = arr[j];
-//         arr[j] = temp;
-//       }
-//     }
-//   }
+//   arr.sort((a, b) => a - b);
 //   console.log(arr);
-//   console.log(arr[0], ("найменше"));
+//   console.log(("Мінімальне число:"), arr[0]);
 // }
