da462db7eb1382c48799fd2319f8b3022c23d538
Kyle553 <elkorda204@gmail.com>
2025-02-12 07:28:25 +0200

    Перемішення файлів

--- Клієнтський JavaScript/Модуль 1 Основи JavaScript/Урок 6 - 7/script2.js
+++ Клієнтський JavaScript/Модуль 1 Основи JavaScript/Урок 6 - 7/script2.js
@@ -2,6 +2,6 @@ function srt(a) {
 return a.split(" ").sort((a, b) => parseInt(a) - parseInt(b)).join(" ");
 };
 
-let str = "12 56 30 2 0 11 8 93 29 16 40 36 12 69 33 99"
+let str = "12 56 30 2 0 11 8 93 29 16 40 36 12 69 33 99";
 str = srt(str);
 console.log(str);
