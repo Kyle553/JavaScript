7572dbca46137ca3102bae34f50a4a09a198f30d
Kyle553 <elkorda204@gmail.com>
2025-02-13 05:30:12 +0200

    Завдання 4

--- Клієнтський JavaScript/Модуль 1 Основи JavaScript/Завдання модуля/Завдання 4/script.js
+++ Клієнтський JavaScript/Модуль 1 Основи JavaScript/Завдання модуля/Завдання 4/script.js
@@ -1,36 +1,20 @@
-// Напишіть програму, яка запитує у користувача три числа і визначає найбільше.
-// щоб функція могла приймати скільки завгодно чисел і визначити з них найбільше 
-const readline = require("readline"); // require("readline") — підключає вбудований в Node.js модуль readline для роботи з введенням та виведенням у терміналі
+const readline = require("readline");
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-let arr = [];
-
 function askQuestion() {
-  rl.question("Введіть число: ", function (value) {
-    // let regex = /^[\d]+$/
-    // зробити щоб пропуськало тільки цифри 
-    // і щоб записувались цифри
-    if (value.toLowerCase() === "stop") {
-      if (arr.length === 0) {
-        console.log("Ви не ввели жодного числа");
-        rl.close();
-      } else {
-        console.log("Ваші числа:", arr);
-        rl.close();
-      }
-    } else if (value.trim() === "") {
-        askQuestion();
-      } else {
-        arr.push(value.trim());
-        askQuestion();
-      }
+  rl.question("", function (value) {
+    value = value.trim().split(/\s+/);
+    console.log("Ваші числа: \n", value.map(Number).sort((a, b) => a - b));
+    const maxNum = Math.max(...value);
+    console.log("Найбільше число:", maxNum);
+
+    rl.close();
   });
 };
 
-console.log("Щоб закрити інтерфейс вводу напишіть stop")
+console.log("Введіть числа через пробіл:");
 askQuestion();
-
