63415a200f1845051a339798ca4f23a61eb50d13
Kyle553 <elkorda204@gmail.com>
2025-03-02 19:13:40 +0200

    Урок 12

--- Клієнтський JavaScript/Модуль 3 Асинхронний JavaScript/Урок 12/script.js
+++ Клієнтський JavaScript/Модуль 3 Асинхронний JavaScript/Урок 12/script.js
@@ -1,36 +1,56 @@
-// змінна яка постійно оновлюється і зберігає час
-
 const pt1 = document.querySelector(".pt1");
 const pt2 = document.querySelector(".pt2");
 const pt3 = document.querySelector(".pt3");
+const pt4 = document.querySelector(".pt4");
 
+const div1 = document.querySelector(".div1");
+
+// ===========================================================================
+const timeHMS = (t) => {
+  setInterval(() => {
+    const time = new Date();
+    t(`${time.getHours()}.${time.getMinutes()}.${time.getSeconds()}`);
+  }, 1000);
+};
+
+timeHMS((time) => {
+  pt2.textContent = time;
+});
+
+// ===========================================================================
 setTimeout(() => {
-  pt1.textContent = "Пройшло 2 секунди"
+  pt1.textContent = "Пройшло 2 секунди";
 }, 2000);
 
-setInterval(() => {
-  time = new Date();
-  pt2.textContent = `${time.getHours()}.${time.getMinutes()}.${time.getSeconds()}`;
+// ===========================================================================
+let i1 = 0;
+const si1 = setInterval(() => {
+  i1++;
+  if (i1 <= 10) {
+    pt3.textContent = i1;
+  } else {
+      clearInterval(si1);
+      pt3.textContent = "10 Таймер зупинився";
+  }
 }, 1000);
 
-// let i = 0;
-// const st1 = setInterval(() => {
-//     i++;
-//     if (i <= 10) {
-//       pt3.textContent = `${time.getSeconds()}`;
-//     } else {
-//         clearInterval(st1);
-//       }
-//   }, 1000);
+// ===========================================================================
+let i2 = 0;
+const si2 = setInterval(() => {
+  i2++;
+  if (i2 < 20) {
+    pt4.textContent = i2;
+  } else {
+      clearInterval(si2);
+      pt4.textContent = `${i2} Час сплив`;
+  }
+}, 1000);
 
-// time = new Date();
-
-
-// Завдання 3: Зупинка інтервалу
-// Напишіть програму, яка через кожні 3 секунди виводить в консоль повідомлення, але після 10 секунд припиняє виконання цього інтервалу.
-
-// Завдання 4: Таймер з динамічним зупиненням
-// Створіть програму, яка за допомогою setTimeout виводить повідомлення "Таймер завершений!", але дозволяє користувачеві через 2 секунди зупинити таймер вручну, натиснувши кнопку "Скасувати".
-
-// Завдання 5: Лічильник з таймером
-// Створіть програму, яка запускає лічильник, який збільшується на 1 кожну секунду. Лічильник повинен зупинятися після 10 секунд.
+div1.addEventListener("click", () => {
+  if (i2 <= 3) {
+    pt4.textContent = `${i2} Ще не можна зупинити таймер`;
+  } else if (i2 > 3) {
+      clearInterval(si2);
+      pt4.textContent = `${i2} Таймер зупинено`;
+  }
+});
