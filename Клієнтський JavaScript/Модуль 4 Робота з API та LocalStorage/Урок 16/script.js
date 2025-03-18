48550d3a3f241cb82744487a168924d60d972760
Kyle553 <elkorda204@gmail.com>
2025-03-18 08:19:40 +0200

    Урок 16 - 17

--- Клієнтський JavaScript/Модуль 4 Робота з API та LocalStorage/Урок 16/script.js
+++ Клієнтський JavaScript/Модуль 4 Робота з API та LocalStorage/Урок 16/script.js
@@ -1,12 +1,12 @@
 // GET =================================================================================
 async function GET() { 
   try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {method: "GET"})
-    console.log("1", response);
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts?userId=4", {method: "GET"})
+    console.log("GET 1", response);
     const data = await response.json();
-    console.log("2",data);
+    console.log("GET 2",data);
   } catch (error) {
-    console.log("1 Помилка", error);
+    console.log("GET 1 Помилка", error);
   }
 }
 
@@ -14,11 +14,11 @@ GET();
 
 fetch(`https://api.thedogapi.com/v1/images/search`, {method: "GET"})
   .then(response => {
-    console.log("3", response);
+    console.log("GET 3", response);
     return response.json();
   })
-  .then(data => console.log("4", data))
-  .catch(error => console.log("2 Помилка", error));
+  .then(data => console.log("GET 4", data))
+  .catch(error => console.log("GET 2 Помилка", error));
 
 // POST =================================================================================
 async function POST() {
@@ -34,14 +34,40 @@ async function POST() {
         userId: 12
       })
     });
-    console.log("5", response);
+    console.log("POST 5", response);
     const data = await response.json();
-    console.log("6", data);
+    console.log("POST 6", data);
   } catch (error) {
-    console.log("3 Помилка", error);
+    console.log("POST 3 Помилка", error);
   }
 }
 
 POST();
 
+// DELETE =================================================================================
+fetch("https://jsonplaceholder.typicode.com/posts/55", {method: "DELETE"})
+.then((response) => {
+  if (!response.ok) {
+    console.log(response.status);
+  } else {
+    console.log("DELETE 7", response);
+    return response;
+  }
+})
+.catch(error => console.error("DELETE 4 Помилка", error));
+
 // PUT =================================================================================
+fetch("https://jsonplaceholder.typicode.com/posts/20", {
+  method: "PUT",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    title: "TEXT1",
+    body: "TEXT2",
+    userId: 12
+  })
+})
+  .then(response => {return response.json()})
+  .then(data => console.log("PUT 8", data))
+  .catch(error => console.error("PUT 5 Помилка", error));
