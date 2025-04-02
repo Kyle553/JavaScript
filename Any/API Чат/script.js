13f12849e283104af39f16d6b59381ff080f2147
Kyle553 <elkorda204@gmail.com>
2025-04-02 22:39:46 +0300

    API чат (не закінчено)

--- Any/API Чат/script.js
+++ Any/API Чат/script.js
@@ -1,5 +1,6 @@
 // createUserName =================================================================================================================
 async function createUserName () {
+  let data = null
   try {
     const response = await fetch("http://127.0.0.1:5173/login", {
       method: "POST",
@@ -9,18 +10,24 @@ async function createUserName () {
       body: JSON.stringify({
         username: `${inpText}`
       })
-    })
-    const data = await response.json();
-    localStorage.setItem("FD_ID", `${data.id}`);
-    console.log(data);
-    
+    });
+
+    data = await response.json();
   } catch (error) {
       console.error(`CATCH ERROR: `, error.message)
   }
+  
+  console.log("createUserName: ", data);
+
+  localStorage.setItem("FD_ID", `${data.id}`);
+
+  inp1.placeholder = "Введіть текст";
+  inp1.value = "";
 }
 
 // POST_MSG =================================================================================================================
 async function POST_MSG () {
+  let data = null;
   try {
     const response = await fetch("http://127.0.0.1:5173/msg", {
       method: "POST",
@@ -31,102 +38,90 @@ async function POST_MSG () {
         id: `${FD_ID}`,
         msg: `${inpText}`
       })
-    })
-    const data = await response.json();
-    localStorage.setItem("LAST_MSG", `${data.msg}`);
-    localStorage.setItem("LAST_ID_MSG", `${data.id}`);
-    localStorage.setItem("LAST_DATE_MSG", `${data.date}`);
+    });
 
-    console.log(data);
-    
+    data = await response.json();
   } catch (error) {
       console.error(`CATCH ERROR: `, error.message)
   }
+
+  console.log("POST_MSG: ", data);
+
+  localStorage.setItem("LAST_MSG", `${data.msg}`);
+  localStorage.setItem("LAST_ID_MSG", `${data.id}`);
+  localStorage.setItem("LAST_DATE_MSG", `${data.date}`);
+
+  inp1.value = "";
 }
 
 // GET_LAST_MSG =================================================================================================================
 async function GET_LAST_MSG () {
+  let data = null;
   try {
-    const response = await fetch("http://127.0.0.1:5173/msg", {
-      method: "GET"})
+    const response = await fetch("http://127.0.0.1:5173/msg", {method: "GET"});
+
     if (!response.ok) {
       throw new Error(`CODE ERROR: ${response.status}`);
     } 
 
-    const data = await response.json();
-    console.log(data);
-
-    const all_msg = data.map((data) => ({
-      username: data.user.username,
-      msg_id: data.id,
-      msg: data.msg,
-      date: data.date
-    }));
-
-    return all_msg;
-
+    data = await response.json();
   } catch (error) {
       console.error(`CATCH ERROR: `, error.message)
   }
-};
-// setInterval(() => {
-//   GET_LAST_MSG();
+
+  console.log("GET_LAST_MSG: ", data);
   
-// }, 5000);
+  const all_msg = data.map((data) => ({
+    username: data.user.username,
+    msg_id: data.id,
+    msg: data.msg,
+    date: data.date
+  }));
+
+  inp1.value = "";
+
+  return all_msg;
+};
 
 // GET_USER =================================================================================================================
 
 async function GET_USER () {
+  let data = null;
   try {
-    const response = await fetch(`http://127.0.0.1:5173/user/${localStorage.getItem("FD_ID")}`, {
-      method: "GET"})
+    const response = await fetch(`http://127.0.0.1:5173/user/${localStorage.getItem("FD_ID")}`, {method: "GET"});
+
     if (!response.ok) {
       throw new Error(`CODE ERROR: ${response.status}`);
     } 
 
-    const data = await response.json();
-    console.log("USER: ", data);
-
-    inp1.placeholder = "Введіть текст";
-    inp1.value = "";
-    if (data.code) {
-      return await createUserName();
-    }
+    data = await response.json();
   } catch (error) {
       console.error(`CATCH ERROR: `, error.message)
   }
+
+  console.log("GET_USER: ", data);
+
+  inp1.value = "";
 };
 
 
-// input ===================================================================================== 
+// input =====================================================
 const inp1 = document.querySelector(".inp");
 
 let inpText = "";
-inp1.addEventListener("input", (e) => {
-  inpText = e.target.value;
+inp1.addEventListener("input", (event) => {
+  inpText = event.target.value;
 });
 
+// button =====================================================
 let div1 = document.querySelector(".div1");
 
 div1.addEventListener("click", async () => {
-  await GET_USER();
-  await POST_MSG();
-  await GET_LAST_MSG();
-  
-
+  // createUserName();
+  //
+  //
+  //
+  //
 });
 
 let FD_ID = localStorage.getItem("FD_ID");
-
-// // HISTORY_MSG ===================================================================================== 
-// async function HISTORY_MSG () {
-//   let all_msg = await GET_LAST_MSG();
-//   let temp_all_msg = all_msg;
-
-
-
-
-
-// };
-
-// HISTORY_MSG();
