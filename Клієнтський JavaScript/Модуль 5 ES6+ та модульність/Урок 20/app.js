ed509bda8664491f073c861fe2115df2e304cf3c
Kyle553 <elkorda204@gmail.com>
2025-04-16 05:09:56 +0300

    Урок 20

---
+++ Клієнтський JavaScript/Модуль 5 ES6+ та модульність/Урок 20/app.js
@@ -0,0 +1,6 @@
+import newCube from "./mathUtils.js";
+import { E as newE, multiply } from "./mathUtils.js";
+
+console.log("multiply(newE, 3): ", multiply(newE, 3));
+
+console.log("newCube(2): ", newCube(2));
---
+++ Клієнтський JavaScript/Модуль 5 ES6+ та модульність/Урок 20/mathUtils.js
@@ -0,0 +1,11 @@
+export default function cube (x) {
+  return x * x * x;
+};
+// export default cube;
+
+export const E = 2.71;
+export function multiply (a, b) {
+  return a * b;
+};
+// export { E, multiply };
+
