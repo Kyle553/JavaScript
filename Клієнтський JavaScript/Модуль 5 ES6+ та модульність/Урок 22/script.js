9d328862edff2e5ca35628e7893da38da0d62812
Kyle553 <elkorda204@gmail.com>
2025-04-23 16:27:11 +0300

    Завдання 22

---
+++ Клієнтський JavaScript/Модуль 5 ES6+ та модульність/Урок 22/script.js
@@ -0,0 +1,43 @@
+const inputField = `Супер секрет`;
+
+function createSecret ( secret ) {
+  const newSecret = secret;
+
+  return function getSecret () {
+    return newSecret;
+  }
+}
+
+const user = createSecret(inputField);
+console.log(user());
+
+// ==============================================
+function createCounter () {
+  let count = 0;
+
+  return {
+    increment(value) {
+      count = count + value;
+      return this;
+    },
+    
+    decrement (value) {
+      count = count - value;
+      return this;
+    },
+    
+    getValue () {
+      console.log(count);
+      return this;
+    },
+
+    reset () {
+      count = 0;
+      console.log(`Лічильник скинуто`);
+      return this;
+    }
+  } 
+}
+
+const counter = createCounter();
+counter.increment(100).decrement(50).getValue();
